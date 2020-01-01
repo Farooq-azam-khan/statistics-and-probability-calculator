7dcc2cbee60fe57c7402bc390ffcb0e2d974afbe
farooq azam khan <farooq1.khan@ryerson.ca>
2020-01-01 11:51:38 -0500

    doing react bootstrap

--- src/components/Distributions.js
+++ src/components/Distributions.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Select from "react-select";
+
+const operationOptions = [
+  { value: "=", label: "=" },
+  { value: "<=", label: "<=" },
+  { value: ">=", label: ">=" },
+  { value: "<", label: "<" },
+  { value: ">", label: ">" }
+];
 
 export default class Distributions extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      randomVariable: "X",
       lower: 0,
       upper: 1,
       operation: "<",
@@ -16,40 +26,74 @@ export default class Distributions extends React.Component {
   };
 
   handleLower = e => {
-    this.setState({ lower: e.target.value });
-    this.calculateProbability();
+    this.setState({ lower: e.target.value }, () => {
+      this.calculateProbability();
+    });
   };
 
   handleUpper = e => {
-    this.setState({ upper: e.target.value });
-    this.calculateProbability();
-  };
-  handleOperation = e => {
-    this.setState({ operation: e.target.value });
-    this.calculateProbability();
+    this.setState({ upper: e.target.value }, () => {
+      this.calculateProbability();
+    });
   };
 
+  handleOperation = operation => {
+    this.setState({ operation }, () => {
+      console.log(`Option selected:`, this.state.operation);
+      this.calculateProbability();
+    });
+  };
+
+  handleRandomVar = e => {
+    this.setState({ randomVariable: e.target.value });
+  };
   render() {
     return (
       <div>
         <h2>Calculate Probability</h2>
-        P (
-        <input
-          type="number"
-          value={this.state.lower}
-          onChange={this.handleLower}
-        />{" "}
-        <input
-          type="text"
-          value={this.state.operation}
-          onChange={this.handleOperation}
-        />{" "}
-        <input
-          type="number"
-          value={this.state.upper}
-          onChange={this.handleUpper}
-        />
-        ) = <span>{this.state.output}</span>
+        <div>
+          <label for="rand-var">Random Variable:</label>{" "}
+          <input
+            id="rand-var"
+            type="text"
+            value={this.state.randomVariable}
+            onChange={this.handleRandomVar}
+          />
+        </div>
+        <div styles={{ margin: "1rem" }}>
+          <label for="select-op">Select Value: </label>
+          <Select
+            id="select-op"
+            value={this.state.operation}
+            onChange={this.handleOperation}
+            options={operationOptions}
+          />
+          <label for="upper-bound-value">Upper value:</label>{" "}
+          <input
+            id="upper-bound-value"
+            type="number"
+            value={this.state.upper}
+            onChange={this.handleUpper}
+          />
+          <div
+            style={{
+              display: "flex",
+              backgroundColor: "lightblue",
+              margin: "2rem",
+              padding: "1rem",
+              textAlign: "center",
+              justifyContent: "center"
+            }}
+          >
+            <div>
+              P(
+              {this.state.randomVariable +
+                this.state.operation +
+                this.state.upper}
+              ) = {this.state.output}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
