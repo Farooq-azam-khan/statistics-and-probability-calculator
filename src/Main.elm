c71012948c70cb006008b81b7806db7f0afd85a8
Farooq Azam Khan <khanazamfarooq99@gmail.com>
2021-01-25 16:57:47 -0500

    Update Main.elm

--- src/Main.elm
+++ src/Main.elm
@@ -1,26 +1,118 @@
 module Main exposing (main)
 
-import BinomialDistribution as BD exposing (Binomial, create_binomial)
 import Browser
-import GeometricDistribution as GD exposing (Geometric)
+import Debug exposing (toString)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
-import PoissionDistribution as PD exposing (Poission)
 
 
-type Distributions
-    = Binomial Int Float
-    | Geometric Float
-    | Poission Float
+type Probability
+    = Probability Float
+
+
+type Lambda
+    = Lambda Float
+
+
+type Distribution
+    = Binomial Int Probability
+    | Geometric Probability
+    | Poission Lambda
+
+
+e : Float
+e =
+    2.718281828459045
+
+
+mean : Distribution -> Float
+mean d =
+    case d of
+        Binomial _ (Probability p) ->
+            p
+
+        Geometric (Probability p) ->
+            1 / p
+
+        Poission (Lambda l) ->
+            l
+
+
+variance : Distribution -> Float
+variance d =
+    case d of
+        Binomial _ (Probability p) ->
+            (1 - p) * p
+
+        Geometric (Probability p) ->
+            (1 - p) / (p * p)
+
+        Poission (Lambda l) ->
+            l
+
+
+factorial : Int -> Int
+factorial n =
+    if n <= 1 then
+        1
+
+    else
+        n * factorial (n - 1)
+
+
+choose : Int -> Int -> Int
+choose n k =
+    if k > n then
+        n
+
+    else
+        factorial n // (factorial n * factorial (n - k))
+
+
+probability : Distribution -> Int -> Float
+probability d x =
+    case d of
+        Binomial n (Probability p) ->
+            let
+                c1 =
+                    toFloat (choose n x)
+
+                c2 =
+                    p ^ Basics.toFloat x
+
+                c3 =
+                    (1 - p) ^ Basics.toFloat (n - x)
+            in
+            c1 * c2 * c3
+
+        Geometric (Probability p) ->
+            let
+                c1 =
+                    (1 - p) ^ (toFloat x - 1)
+            in
+            c1 * p
+
+        Poission (Lambda lamb) ->
+            let
+                c1 =
+                    lamb ^ toFloat x
+
+                c2 =
+                    e ^ (-1 * lamb)
+            in
+            (c1 * c2) / toFloat (factorial x)
 
 
 type alias Model =
-    { selected_distribution : Distributions }
+    { selected_distribution : Distribution }
 
 
 type Msg
     = NoOp
+    | SelectBinomial
+    | SelectGeometric
+    | SelectPoission
 
 
 main : Program () Model Msg
@@ -38,7 +130,7 @@ main =
 
 init : Model
 init =
-    { selected_distribution = Poission 0.1 }
+    { selected_distribution = Poission (Lambda 0.1) }
 
 
 
@@ -51,13 +143,28 @@ update msg model =
         NoOp ->
             model
 
+        SelectBinomial ->
+            { model | selected_distribution = Binomial 0 (Probability 0) }
+
+        SelectGeometric ->
+            { model | selected_distribution = Geometric (Probability 0.1) }
+
+        SelectPoission ->
+            { model | selected_distribution = Poission (Lambda 0.1) }
+
 
 view : Model -> Html Msg
 view model =
     div []
         [ h1 []
             [ text "Statistics and Probability Calculator" ]
-        , p
+        , span [] [ text "Selected Distribution" ]
+        , div []
+            [ button [ onClick SelectGeometric ] [ text "Geometric" ]
+            , button [ onClick SelectBinomial ] [ text "Binomial" ]
+            , button [ onClick SelectPoission ] [ text "Poission" ]
+            ]
+        , h2
             []
             [ case model.selected_distribution of
                 Geometric _ ->
@@ -68,5 +175,13 @@ view model =
 
                 Poission _ ->
                     text "Poission"
+            ]
+        , div []
+            [ text "Mean: "
+            , text (toString (mean model.selected_distribution))
+            ]
+        , div []
+            [ text "Variance: "
+            , text (toString (variance model.selected_distribution))
             ]
         ]
